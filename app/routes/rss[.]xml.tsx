c01d52017f69935036a3684c831214dda30b2f39
mobregozo <obregozo.manuel@gmail.com>
2024-10-30 14:23:45 +0100

    fix: rss feed with no image

--- app/routes/rss[.]xml.tsx
+++ app/routes/rss[.]xml.tsx
@@ -43,6 +43,5 @@ const renderItem = (article: PostProperties) => {
                 day: "2-digit",
               }).format(new Date(article.date))
             ).toISOString()}</pubDate>
-            <enclosure url="${article.photoURL}" type="image/jpeg" />
         </item>`;
 };
