c094908c8891d95c68a6faf2fbc654ca3f610b7d
mobregozo <obregozo.manuel@gmail.com>
2024-12-15 10:29:13 +0100

    Revert "chore: deployment to vercel"
    This reverts commit 8eb7124c76745c1e720070f2d6b51f24d118e8d1.

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ isSsrBuild, command }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-          input: "./server/app.ts",
-        }
-      : undefined,
-  },
-  ssr: {
-    noExternal: command === "build" ? true : undefined,
-  },
+export default defineConfig({
   plugins: [tsconfigPaths(), tailwindcss(), reactRouter()],
-}));
+});
