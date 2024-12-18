9055f628dd0b9cc8a2bbc2871582332a13bb67ed
mobregozo <obregozo.manuel@gmail.com>
2024-12-18 17:46:18 +0100

    add case fot twitter embed links

--- app/components/post-embed.tsx
+++ app/components/post-embed.tsx
@@ -5,5 +5,19 @@ type PostVideoProps = {
 };
 
 export const PostEmbed = ({ block }: PostVideoProps) => {
+  const url = block.embed?.url;
+
+  if (url.includes("twitter")) {
+    return (
+      <iframe
+        title={block.id}
+        width={300}
+        height={700}
+        className="my-10 aspect-video mx-auto"
+        src={block.embed?.url}
+      ></iframe>
+    );
+  }
+
   return block.embed ? <a href={block.embed.url}>{block.embed.url}</a> : null;
 };
