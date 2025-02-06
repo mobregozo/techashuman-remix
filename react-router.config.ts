cd478b1b99ca53783e20ca827ab494751ad129bb
Manuel Obregozo Alesso <obregozo.manuel@gmail.com>
2025-02-06 17:36:50 +0100

    new react-router preset

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,10 @@
 import { getLatestArticles } from "./app/utils/read-posts.server";
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
+
 export default {
   ssr: true,
+  presets: [vercelPreset()],
   async prerender() {
     let popularArticles = await getLatestArticles();
     const PopularArticlesPages = popularArticles.map(
