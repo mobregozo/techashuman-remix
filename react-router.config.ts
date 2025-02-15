721629532b5299924543050638f7e0d7fe7b2e66
Manuel Obregozo Alesso <obregozo.manuel@gmail.com>
2025-02-15 11:35:52 +0100

    fix: update dependencies

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,8 @@
 import { getLatestArticles } from "./app/utils/read-posts.server";
 import type { Config } from "@react-router/dev/config";
-import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   ssr: true,
-  presets: [vercelPreset()],
   async prerender() {
     let popularArticles = await getLatestArticles();
     const PopularArticlesPages = popularArticles.map(
